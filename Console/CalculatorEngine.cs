7e37f5d122e89c719ef45567a7af2790d4a9dbe1
Moha Med <mohamedamjformation@gmail.com>
2019-11-28 11:19:28 +0100

    Initialize commit

---
+++ Console/CalculatorEngine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    public class CalculatorEngine
+    {
+        /// <summary>
+        /// Fonction to Addition two values.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Add(int a, int b)
+        {
+            return a+b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a-b;
+        }
+        public int Mul(int a, int b)
+        {
+            return a*b;
+        }
+        public int Div(int a, int b)
+        {
+            int res;
+
+            if(b > 0)
+            {
+                res = a / b;
+            }
+            else
+            {
+                res = Int32.MinValue;
+            }
+            return res;
+        }
+    }
+}
